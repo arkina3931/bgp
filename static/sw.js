cacd262dd3813287fd5f9a5a4e12f34f021d1caf
zhangyi <zhangyi@bgp.local>
2026-06-16 16:21:33 +0800

    feat: add PWA service worker for offline caching

---
+++ static/sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'bgp-v1';
+var STATIC_ASSETS = [
+    '/static/common.css',
+    '/static/common.js',
+    '/static/components.js',
+    '/',
+    '/gamelist'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(STATIC_ASSETS).catch(function () {});
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        caches.match(event.request).then(function (cached) {
+            var fetchPromise = fetch(event.request).then(function (response) {
+                if (response.ok) {
+                    var clone = response.clone();
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, clone);
+                    });
+                }
+                return response;
+            }).catch(function () {
+                return cached;
+            });
+            return cached || fetchPromise;
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(
+                keys.filter(function (k) { return k !== CACHE_NAME; })
+                    .map(function (k) { return caches.delete(k); })
+            );
+        })
+    );
+});
